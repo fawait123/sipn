8a73639eb1f3165829e6625c87c1c6efb575fff2
fawait626 <fawait626gmail.com>
2022-12-24 21:09:58 +0700

    update navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,7 +3,7 @@
         <div class="nk-sidebar-menu" data-simplebar>
             <ul class="nk-menu">
                 <li class="nk-menu-heading">
-                    <h6 class="overline-title text-primary-alt">Dashboards</h6>
+                    <h6 class="overline-title text-primary-alt">Dashboard</h6>
                 </li><!-- .nk-menu-item -->
                 <li class="nk-menu-item">
                     <a href="{{ route('home') }}" class="nk-menu-link">
@@ -16,49 +16,49 @@
                 </li><!-- .nk-menu-heading -->
                 <li class="nk-menu-item">
                     <a href="{{ route('kepalasekolah.index') }}" class="nk-menu-link">
-                        <span class="nk-menu-icon"><em class="icon ni ni-dashlite"></em></span>
+                        <span class="nk-menu-icon"><em class="icon ni ni-layers"></em></span>
                         <span class="nk-menu-text">Kepala Sekolah </span>
                     </a>
                 </li><!-- .nk-menu-item -->
                 <li class="nk-menu-item">
                     <a href="{{ route('guru.index') }}" class="nk-menu-link">
-                        <span class="nk-menu-icon"><em class="icon ni ni-dashlite"></em></span>
+                        <span class="nk-menu-icon"><em class="icon ni ni-block-over"></em></span>
                         <span class="nk-menu-text">Guru </span>
                     </a>
                 </li><!-- .nk-menu-item -->
                 <li class="nk-menu-item">
                     <a href="{{ route('mapel.index') }}" class="nk-menu-link">
-                        <span class="nk-menu-icon"><em class="icon ni ni-dashlite"></em></span>
+                        <span class="nk-menu-icon"><em class="icon ni ni-grid-alt"></em></span>
                         <span class="nk-menu-text">Mapel </span>
                     </a>
                 </li><!-- .nk-menu-item -->
                 <li class="nk-menu-item">
                     <a href="{{ route('prodi.index') }}" class="nk-menu-link">
-                        <span class="nk-menu-icon"><em class="icon ni ni-dashlite"></em></span>
+                        <span class="nk-menu-icon"><em class="icon ni ni-tile-thumb"></em></span>
                         <span class="nk-menu-text">Prodi </span>
                     </a>
                 </li><!-- .nk-menu-item -->
                 <li class="nk-menu-item">
                     <a href="{{ route('siswa.index') }}" class="nk-menu-link">
-                        <span class="nk-menu-icon"><em class="icon ni ni-dashlite"></em></span>
+                        <span class="nk-menu-icon"><em class="icon ni ni-coins"></em></span>
                         <span class="nk-menu-text">Siswa </span>
                     </a>
                 </li><!-- .nk-menu-item -->
                 <li class="nk-menu-item">
                     <a href="{{ route('ekskul.index') }}" class="nk-menu-link">
-                        <span class="nk-menu-icon"><em class="icon ni ni-dashlite"></em></span>
+                        <span class="nk-menu-icon"><em class="icon ni ni-tranx"></em></span>
                         <span class="nk-menu-text">Ekskul </span>
                     </a>
                 </li><!-- .nk-menu-item -->
                 <li class="nk-menu-item">
                     <a href="{{ route('ajaran.index') }}" class="nk-menu-link">
-                        <span class="nk-menu-icon"><em class="icon ni ni-dashlite"></em></span>
+                        <span class="nk-menu-icon"><em class="icon ni ni-view-col"></em></span>
                         <span class="nk-menu-text">Ajaran </span>
                     </a>
                 </li><!-- .nk-menu-item -->
                 <li class="nk-menu-item">
                     <a href="{{ route('wali.index') }}" class="nk-menu-link">
-                        <span class="nk-menu-icon"><em class="icon ni ni-dashlite"></em></span>
+                        <span class="nk-menu-icon"><em class="icon ni ni-card-view"></em></span>
                         <span class="nk-menu-text">Wali </span>
                     </a>
                 </li><!-- .nk-menu-item -->
