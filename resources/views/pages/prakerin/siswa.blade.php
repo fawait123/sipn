0c68ae043f1ea27e6fa6ea6e6b3b12f491fe2103
fawait626 <fawait626gmail.com>
2023-02-04 20:21:18 +0700

    update terbaru

--- resources/views/pages/prakerin/siswa.blade.php
+++ resources/views/pages/prakerin/siswa.blade.php
@@ -22,7 +22,8 @@
                             <div class="nk-block-head nk-block-head-sm">
                                 <div class="nk-block-between">
                                     <div class="nk-block-head-content">
-                                        <h3 class="nk-block-title page-title">Siswa Didik {{ $row->guru->nm_guru }}</h3>
+                                        <h3 class="nk-block-title page-title">Siswa Didik {{ $row->guru->nm_guru }} Kelas
+                                            {{ $row->tingkat }}</h3>
                                         <div class="nk-block-des text-soft">
                                             <p>Kamu mempunyai {{ count($siswa) }} siswa didik</p>
                                         </div>
